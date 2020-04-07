787f44bc7e5fd46939978c4e350fbb2bd9f363b2
Kousha Talebian <kousha.talebian@gmail.com>
2020-04-07 11:51:53 -0700

    remove build

--- build/index.d.ts
+++
@@ -1,6 +0,0 @@
-export default class ModuleScopePlugin {
-    private readonly appSources;
-    private readonly allowedFiles;
-    constructor(appSrc: string, allowedFiles?: string[]);
-    apply(compiler: any): void;
-}
